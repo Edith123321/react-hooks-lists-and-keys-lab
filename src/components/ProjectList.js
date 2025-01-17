83bf9ed40fdf4e7e48d1208c92af0c24fc41099f
edith.githinji <edithgithinji2020@gmail.com>
2025-01-17 16:37:53 +0300

    projectslist

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,22 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log(projects); // Log the projects array for debugging
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* Render ProjectItem components for each project in the array */}
+        {projects.map((project, index) => (
+          <ProjectItem
+            key={index}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
